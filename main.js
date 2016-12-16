5bbd10b0af7b3c1b09db1716f6237556f8a8f0d4
Jordan Last <jordan.michael.last@gmail.com>
2016-12-16 12:05:00 -0700

    transpilation and non transpilation working

--- main.js
+++ main.js
@@ -6,31 +6,47 @@ createServer(builder);
 function createServer(builder, httpVersion, outputDir) {
     const static = require('node-static');
     const fileServer = new static.Server(process.cwd());
-    const httpServer = httpVersion === '2' ? createHTTP2Server(builder) : createHTTPServer(builder);
+    const httpServer = httpVersion === '2' ? createHTTP2Server(builder, fileServer) : createHTTPServer(builder, fileServer);
     httpServer.listen(8000, (error) => {
         if (error) console.log(error);
         console.log('zwitterion server listening on port 8000');
     });
 }
 
-function createHTTPServer(builder) {
+function createHTTPServer(builder, fileServer) {
     return require('http').createServer((req, res) => {
         const absoluteFilePath = `${process.cwd()}${req.url}`;
         const relativeFilePath = req.url.slice(1);
-        const transpilation = transpilations[relativeFilePath];
+        const fileExtension = relativeFilePath.slice(relativeFilePath.lastIndexOf('.'));
 
-        if (transpilation) {
-            res.end(transpilation);
-        }
-        else {
-            builder.buildStatic(relativeFilePath).then((output) => {
-                transpilations[relativeFilePath] = output.source;
-                res.end(output.source);
-            }, (error) => {
-                console.log(error);
-            });
-        }
+        fileExtension === '.ts' ? buildAndServe(res, relativeFilePath) : serveWithoutBuild(fileServer, req, res);
     });
+}
+
+function buildAndServe(res, relativeFilePath) {
+    const transpilation = transpilations[relativeFilePath];
+
+    if (transpilation) {
+        res.end(transpilation);
+    }
+    else {
+        builder.buildStatic(relativeFilePath).then((output) => {
+            transpilations[relativeFilePath] = output.source;
+            res.end(output.source);
+        }, (error) => {
+            console.log(error);
+        });
+    }
+}
+
+function serveWithoutBuild(fileServer, req, res) {
+    req.addListener('end', () => {
+        fileServer.serve(req, res, (error, result) => {
+            if (error && error.status === 404) {
+                fileServer.serveFile('/index.html', 200, {}, req, res)
+            }
+        });
+    }).resume();
 }
 
 function createHTTP2Server(builder) {
--- test.ts
+++
@@ -1,1 +0,0 @@
-alert('it worked my friend');
