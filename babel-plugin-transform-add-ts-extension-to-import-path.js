93609024b4eb84041d56d67d7ba7bb67c07ece25
Jordan Last <jordan.michael.last@gmail.com>
2018-03-27 17:15:06 -0600

    do not try to add another .ts extension if it is already there

--- babel-plugin-transform-add-ts-extension-to-import-path.js
+++ babel-plugin-transform-add-ts-extension-to-import-path.js
@@ -3,10 +3,11 @@ module.exports = {
         'ImportDeclaration': (path) => {
             const isRelativePath = path.node.source.value.indexOf('.') === 0;
             const hasJSExtension = path.node.source.value.length - path.node.source.value.lastIndexOf('.js') === 3;
+            const hasTSExtension = path.node.source.value.length - path.node.source.value.lastIndexOf('.ts') === 3;
             const hasExtraJSExtension = path.node.source.value.indexOf('.ts.js') !== -1;
             const hasExtraTSExtension = path.node.source.value.indexOf('.js.ts') !== -1;
 
-            path.node.source.value = hasExtraTSExtension ? path.node.source.value.replace('.ts', '') : hasExtraJSExtension ? path.node.source.value.replace('.js', '') : isRelativePath && hasJSExtension ? path.node.source.value : isRelativePath ? `${path.node.source.value}.ts` : path.node.source.value;
+            path.node.source.value = hasExtraTSExtension ? path.node.source.value.replace('.ts', '') : hasExtraJSExtension ? path.node.source.value.replace('.js', '') : isRelativePath && hasJSExtension ? path.node.source.value : isRelativePath && !hasTSExtension ? `${path.node.source.value}.ts` : path.node.source.value;
         }
     }
 };
