29d203f9bcd4c8feb8a8d8d9e1f1c6c4a441c53b
Jordan Last <jordan.michael.last@gmail.com>
2019-12-27 17:41:59 -0800

    try again

--- test/test.ts
+++ test/test.ts
@@ -51,6 +51,16 @@ import { exec } from 'child_process';
 
             <body>
                 <script type="module">
+                    if (self.ZWITTERION_SOCKET.readyState !== 1) {
+                        self.ZWITTERION_SOCKET.addEventListener('open', () => {
+                            self.ZWITTERION_SOCKET.send('STARTING_TESTS');
+
+                        });
+                    }
+                    else {
+                        self.ZWITTERION_SOCKET.send('STARTING_TESTS');
+                    }
+
                     ${topLevelTestDescriptions.map((testDescription: Readonly<TestDescription>) => {
                         return `import * as ${testDescription.id} from './${testDescription.moduleName}';\n`;
                     }).join('')}
@@ -88,7 +98,7 @@ import { exec } from 'child_process';
         fs.writeFileSync(`./${testDescription.moduleName}`, testDescription.moduleContents);
     });
 
-    const chromeCommand: 'chrome' | 'google-chrome' = process.env.OS === 'ubuntu-latest' ? 'google-chrome' : process.env.OS === 'macos-latest' ? '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome' : 'start chrome';
+    const chromeCommand: 'chrome' | 'google-chrome' = process.env.OS === 'ubuntu-latest' ? 'google-chrome' : process.env.OS === 'macos-latest' ? 'open -a "Google Chrome"' : 'start chrome';
 
     const childProcess = exec(`${chromeCommand} --headless --disable-gpu --remote-debugging-port=7777 http://localhost:${commandLineOptions.httpPort}`);
     
