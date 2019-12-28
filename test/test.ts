5b54b8d5a7c8ac77909d39ca949e1ce56ad6b9ac
Jordan Last <jordan.michael.last@gmail.com>
2019-12-27 16:44:07 -0800

    remove all tests on the file system

--- test/test.ts
+++ test/test.ts
@@ -32,8 +32,8 @@ import { exec } from 'child_process';
         ...prepareJavaScriptTestDescriptions(),
         ...prepareTypeScriptTestDescriptions(),
         ...prepareAssemblyScriptTestDescriptions(),
-        ...prepareRustTestDescriptions(),
-        ...prepareCTestDescriptions(),
+        // ...prepareRustTestDescriptions(),
+        // ...prepareCTestDescriptions(),
         // ...prepareCPPTestDescriptions(),
         ...prepareWatTestDescriptions()
     ];
