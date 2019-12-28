7ce7caf9ee8709a3c8e70b26b8bd58a90dd792b6
Jordan Last <jordan.michael.last@gmail.com>
2019-12-27 16:30:21 -0800

    trying firefox for now

--- test/test.ts
+++ test/test.ts
@@ -88,10 +88,10 @@ import { exec } from 'child_process';
         fs.writeFileSync(`./${testDescription.moduleName}`, testDescription.moduleContents);
     });
 
-    const childProcess = exec(`google-chrome --headless --disable-gpu --remote-debugging-port=7777 http://localhost:${commandLineOptions.httpPort}`);
+    // const childProcess = exec(`google-chrome --headless --disable-gpu --remote-debugging-port=7777 http://localhost:${commandLineOptions.httpPort}`);
     
     // TODO add firefox testing
-    // const childProcess = exec(`firefox --headless http://localhost:${commandLineOptions.httpPort}`);
+    const childProcess = exec(`firefox --headless http://localhost:${commandLineOptions.httpPort}`);
 
     const timeoutId: NodeJS.Timeout = setTimeout(() => {
         process.exit(1);
